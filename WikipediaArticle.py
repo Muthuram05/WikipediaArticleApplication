2fec53b2ae6cdc202ee82ddbd0084ea83285ceb2
Muthu Raman <88555538+Muthuram05@users.noreply.github.com>
2022-12-10 18:19:58 +0530

    Added voice Assist

--- WikipediaArticle.py
+++ WikipediaArticle.py
@@ -1,4 +1,16 @@
 import wikipedia
+import pyttsx3
+import speech_recognition as sr
+
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[1].id)
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
 query = "phone"
-results = wikipedia.summary(query, sentences=2)
+wikipedia.set_lang("hi")
+results = wikipedia.summary(query)
 print(results)
+speak(results)
