6d678a111242e4104e8e1e942e140474511952cd
Muthu Raman <88555538+Muthuram05@users.noreply.github.com>
2022-12-14 14:45:03 +0530

    Update WikipediaArticle.py

--- WikipediaArticle.py
+++ WikipediaArticle.py
@@ -2,6 +2,7 @@ import wikipedia
 import pyttsx3
 import speech_recognition as sr
 
+
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
 engine.setProperty('voice', voices[1].id)
@@ -9,8 +10,27 @@ engine.setProperty('voice', voices[1].id)
 def speak(audio):
     engine.say(audio)
     engine.runAndWait()
-query = "phone"
-wikipedia.set_lang("hi")
-results = wikipedia.summary(query)
-print(results)
-speak(results)
+
+def takeCommand():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        r.pause_threshold = 1
+        audio = r.listen(source)
+    try:
+        print("Recognizing...")
+        query = r.recognize_google(audio, language='en-in')
+        print(f"User said: {query}\n")
+    except Exception as e:
+        # print(e)
+        print("Say that again please...")
+        return "None"
+    return query
+
+while True:
+    #query = "phone"
+    query = takeCommand().lower()
+    if(query != ""):
+       results = wikipedia.summary(query)
+       print(results)
+       speak(results)
